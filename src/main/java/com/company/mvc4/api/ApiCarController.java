7b70daad47cd2ffa76b2a344694193786007fe8f
Gusts Linkevics <gusts@gst.lv>
2020-12-28 22:00:55 +0200

    Web API and jQuery

--- src/main/java/com/company/mvc4/api/ApiCarController.java
+++ src/main/java/com/company/mvc4/api/ApiCarController.java
@@ -2,10 +2,9 @@ package com.company.mvc4.api;
 
 import com.company.mvc4.data.Car;
 import com.company.mvc4.data.CarsRepository;
+import com.company.mvc4.dto.CarSearchDto;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
@@ -20,5 +19,10 @@ public class ApiCarController {
     @GetMapping("/cars")
     public Iterable<Car> getCars() {
         return repo.getCars();
+    }
+
+    @PostMapping(value = "/cars", consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public Iterable<Car> searchCars(@RequestBody CarSearchDto searchDto) {
+       return repo.getCars(searchDto);
     }
 }
